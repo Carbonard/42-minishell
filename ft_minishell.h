15bb5fb50eba07def44a8348fa5976f79ee8604c
Carbonard <Carbonard@users.noreply.github.com>
2026-01-17 17:38:43 +0100

    Add separator_enum to header

--- ft_minishell.h
+++ ft_minishell.h
@@ -6,7 +6,7 @@
 /*   By: rselva-2 <rselva-2@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/16 14:57:20 by rselva-2          #+#    #+#             */
-/*   Updated: 2026/01/17 15:33:09 by rselva-2         ###   ########.fr       */
+/*   Updated: 2026/01/17 17:37:12 by rselva-2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,22 @@
 
 # include "libft.h"
 # include <stdio.h>
-// # include <readline.h>
 
 # define MAX_PWD 1024
+
+enum e_separators
+{
+	AND,
+	OR,
+	PIPE
+};
 
 typedef struct s_command
 {
 	struct s_command	*cmd1;
 	struct s_command	*cmd2;
 	char				*cmd;
-	char				sep;
+	int					sep;
 }	t_command;
 
 // Built-ins
